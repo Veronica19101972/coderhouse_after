f28588782c990867f659f1416771242d97605697
Verónica Montaña <veronicamontana@gmail.com>
2023-05-09 10:04:47 -0300

    Primer Commit

---
+++ banco/menu.py
@@ -0,0 +1,8 @@
+def menu_cuenta_bancaria():
+    print("1. Crear cuenta")
+    print("2. Mostrar cuentas")
+    print("3. Ingresar dinero")
+    print("4. Retirar dinero")
+    print("5. Salir")
+    opcion = int(input("Ingrese una opcion: "))
+    return opcion
